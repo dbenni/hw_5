31de1bde04e9f7a3e54a6f4ea09efe30bc1c6499
Daniel <daniel.benninghoff@mavs.uta.edu>
2016-10-09 21:14:51 -0500

    here

---
+++ hw_5/factory_test.cpp
@@ -0,0 +1,15 @@
+#include"std_lib_facilities.h"
+#include "robo_head.h"
+#include"robo_arm.h"
+#include"robo_battery.h"
+#include"robo_motor.h"
+#include"robo_torso.h"
+#include"factory.h"
+
+int main() {
+	factory start;
+	start.factory_menu();
+
+
+
+}
