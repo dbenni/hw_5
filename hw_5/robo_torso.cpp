6638ff92de914f42cc0313de32bcae3e2a1bd962
Daniel <daniel.benninghoff@mavs.uta.edu>
2016-10-10 19:43:17 -0500

    test commit

--- hw_5/robo_torso.cpp
+++ hw_5/robo_torso.cpp
@@ -76,3 +76,32 @@ int robo_torso::getbatn()
 	i = batnum;
 	return i;
 }
+
+void robo_torso::torsodump()
+{
+	cout << "Title  " << name << "\n";
+	cout << "Part Number  " << partn << "\n";
+	cout << "Cost  " << cost << "\n";
+	cout << "Weight  " << weight << "\n";
+	cout << "Short Description  " << "\n" << descrip << "\n";
+	cout << "\n";
+	if (batnum == 1) {
+		cout << "Battery1:  " << "\n";
+		one.battdump();
+	}
+	else if (batnum == 2) {
+		cout << "Battery1:" << "\n";
+		one.battdump();
+		cout << "Battery2:" << "\n";
+		two.battdump();
+	}
+	else if (batnum == 3) {
+		cout << "Battery1:" << "\n";
+		one.battdump();
+		cout << "Battery2:" << "\n";
+		two.battdump();
+		cout << "Battery3:" << "\n";
+		three.battdump();
+
+	}
+}
